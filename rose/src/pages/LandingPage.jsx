65241dcdc7b8f4fd134f610ff95915bdd1d12b2d
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2022-12-16 19:35:28 +0700

    feat: add typewriter & sessionStorage

---
+++ rose/src/pages/LandingPage.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import Typewriter from "typewriter-effect";
+import Rose from "../components/Rose";
+
+export const LandingPage = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  useEffect(() => {
+    const localValue = sessionStorage.getItem("Nama");
+    if (localValue) {
+      setInputValue(localValue);
+    }
+  }, []);
+
+  return (
+    <div className="h-[100vh] bg-red-400">
+      <div className="relative ">
+        <div className="font-bold text-red-50 text-3xl absolute top-[40vh] left-1/3">
+          <Typewriter
+            options={{
+              strings: [`Hi ${inputValue}`],
+              delay: "natural",
+              pauseFor: 5000,
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        </div>
+      </div>
+      <Rose />
+    </div>
+  );
+};
